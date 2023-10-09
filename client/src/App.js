981b70108944b619dc4d08219a7470e796a0d9e5
Serenaaa <sw.online.edu@gmail.com>
2023-10-09 14:19:37 -0700

    resolving dependency issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,38 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import Form from "./components/Form/Form.js";
+import Products from "./components/Products/Products.js";
+import Product from "./components/Products/Product/Product.js";
+
+import useStyles from "./styles";
 
 const App = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h3" align="center">
+          ECC Product Manager
+        </Typography>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={4}
+          >
+            <Grid item xs={12} sm={7}>
+              <Products />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
