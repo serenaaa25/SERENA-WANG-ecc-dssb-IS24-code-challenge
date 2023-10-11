0e49a1034ade39a7de08e0bc8214e2c69a1702f0
Serenaaa <sw.online.edu@gmail.com>
2023-10-11 10:40:10 -0700

    pagination init commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import React, { useState, useEffect } from "react";
-import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import {
+  Container,
+  AppBar,
+  Typography,
+  Grow,
+  Grid,
+  Paper,
+} from "@material-ui/core";
 import Form from "./components/Form/Form.js";
 import Products from "./components/Products/Products.js";
 import { useDispatch } from "react-redux";
 import useStyles from "./styles";
 import { getProducts } from "./actions/products.js";
+import Pagination from "./components/Pagination/Pagination.js";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const App = () => {
   const classes = useStyles();
@@ -16,30 +25,35 @@ const App = () => {
   }, [currentId, dispatch]);
 
   return (
-    <Container maxWidth="lg">
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h3" align="center">
-          ECC Product Manager
-        </Typography>
-      </AppBar>
-      <Grow in>
-        <Container>
-          <Grid
-            container
-            justifyContent="space-between"
-            alignItems="stretch"
-            spacing={4}
-          >
-            <Grid item xs={12} sm={7}>
-              <Products setCurrentId={setCurrentId} />
+    <Router>
+      <Container maxWidth="lg">
+        <AppBar className={classes.appBar} position="static" color="inherit">
+          <Typography className={classes.heading} variant="h3" align="center">
+            ECC Product Manager
+          </Typography>
+        </AppBar>
+        <Grow in>
+          <Container>
+            <Grid
+              container
+              justifyContent="space-between"
+              alignItems="stretch"
+              spacing={4}
+            >
+              <Grid item xs={12} sm={7}>
+                <Products setCurrentId={setCurrentId} />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <Form currentId={currentId} setCurrentId={setCurrentId} />
+                <Paper elevation={6}>
+                  <Pagination />
+                </Paper>
+              </Grid>
             </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
-    </Container>
+          </Container>
+        </Grow>
+      </Container>
+    </Router>
   );
 };
 
